0805f681980485a7c8997261ff7d71d3f4af3bdd
jrg96 <gomezlopez.jorge96@gmail.com>
2020-03-27 11:22:32 -0600

    Display basic blog post information

--- functions.php
+++ functions.php
@@ -14,3 +14,10 @@
 	}
 	
 	add_action('after_setup_theme', 'adv_theme_support');
+	
+	function set_excerpt_length()
+	{
+		return 25;
+	}
+
+	add_filter('excerpt_length', 'set_excerpt_length');
--- index.php
+++ index.php
@@ -1,15 +1,44 @@
 <?php get_header(); ?>
 		<div class="container content">
 			<div class="main block">
-				<article class="post">
-					<h2>Blog Post 1</h2>
-					<p class="meta">Posted at 11:00 on May 9 by admin</p>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc placerat vehicula blandit. Praesent pharetra, libero nec fringilla tincidunt, ex nulla volutpat est, nec lobortis ipsum ligula sed turpis. Curabitur semper vel arcu eu pretium. Pellentesque posuere leo pulvinar molestie congue. Suspendisse in enim elementum, auctor sapien ac, pretium augue. Phasellus sed tortor nec sapien hendrerit molestie. Morbi rutrum rutrum ex id aliquam.
-					</p>
-					
-					<a class="button" href="#">Read More</a>
-				</article>
+				<?php if(have_posts()) : ?>
+					<?php while(have_posts()) : the_post(); ?>
+						<article class="post">
+							<h2><?php the_title(); ?></h2>
+							<p class="meta">
+								Posted at <?php the_date('l jS \of F Y h:i:s A'); ?> 
+								by 
+								<a href="<?php get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a>
+								
+								|
+								
+								Posted In
+								<?php
+									$categories = get_the_category();
+									$separator = ', ';
+									$output = '';
+									
+									if ($categories)
+									{
+										foreach($categories as $category)
+										{
+											$output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>' . $separator;
+										}
+									}
+									
+									echo trim($output, $separator);
+								?>
+							</p>
+							<p>
+								<?php echo the_excerpt(); ?>
+							</p>
+							
+							<a class="button" href="<?php the_permalink(); ?>">Read More</a>
+						</article>
+					<?php endwhile; ?>
+				<?php else : ?>
+					<?php echo wpautop('Sorry no posts were found'); ?>
+				<?php endif; ?>
 			</div>
 			
 			<div class="side">
